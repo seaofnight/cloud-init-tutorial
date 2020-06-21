5e73a8fd225330ad3584b3aa66f70e1db2be40cf
kim youngjin <seaofnight@users.noreply.github.com>
2020-06-21 13:56:08 +0900

    Update create-vm.sh

--- create-vm.sh
+++ create-vm.sh
@@ -1,29 +1,35 @@
 #!/bin/bash 
 
-WORKSPACE=${PWD}
-TEMPLATE=$WORKSPACE/template
+set -ex
 
-CPU=6
-MEM=10240
-HOSTNAME=kf0
-DISK=$HOSTNAME.qcow2
+source ./vm_env
 
-USER_DATA=user-data
-META_DATA=meta-data
-CI_ISO=$HOSTNAME-cidata.iso
+purge_old_object(){
 
-IMAGE=$TEMPLATE/CentOS-7-x86_64-GenericCloud-2003.qcow2
+ rm -rf $WORKSPACE/$HOSTNAME
 
-rm -rf $WORKSPACE/$HOSTNAME 
-mkdir -p $WORKSPACE/$HOSTNAME
-cp $IMAGE $WORKSPACE/$HOSTNAME/$HOSTNAME.qcow2
+}
 
-cp $TEMPLATE/user-data $WORKSPACE/$HOSTNAME/$USER_DATA
-cp $TEMPLATE/meta-data $WORKSPACE/$HOSTNAME/$META_DATA
+generate_data(){
 
-genisoimage -output $WORKSPACE/$HOSTNAME/$CI_ISO -volid cidata -joliet -r $WORKSPACE/$HOSTNAME/$USER_DATA $WORKSPACE/$HOSTNAME/$META_DATA &>> $WORKSPACE/$HOSTNAME/$HOSTNAME-gen-image.log
+ IMAGE=$TEMPLATE/CentOS-7-x86_64-GenericCloud-2003.qcow2
 
-virt-install \
+ mkdir -p $WORKSPACE/$HOSTNAME
+ cp $IMAGE $WORKSPACE/$HOSTNAME/$HOSTNAME.qcow2
+ cp $TEMPLATE/user-data $WORKSPACE/$HOSTNAME/$USER_DATA
+ cat template/meta-data | sed -e "s/HOSTNAME/$HOSTNAME/g" -e "s/ADDRESS/$ADDRESS/g" > $WORKSPACE/$HOSTNAME/$META_DATA
+
+}
+
+generate_ciimage(){
+
+ genisoimage -output $WORKSPACE/$HOSTNAME/$CI_ISO -volid cidata -joliet -r $WORKSPACE/$HOSTNAME/$USER_DATA $WORKSPACE/$HOSTNAME/$META_DATA &>> $WORKSPACE/$HOSTNAME/$HOSTNAME-gen-image.log
+
+}
+
+create_vm() {
+
+ virt-install \
   --memory $MEM \
   --vcpus $CPU \
   --name ${HOSTNAME} \
@@ -33,6 +39,11 @@ virt-install \
   --os-variant centos7.0 \
   --virt-type kvm \
   --graphics none \
-  --network bridge=br0 \
-  --import
+  --network bridge=br0
 
+}
+
+purge_old_object
+generate_data
+generate_ciimage
+create_vm
